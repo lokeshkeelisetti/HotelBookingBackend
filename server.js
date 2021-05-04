0405548f8c7c4cec75ad6829693b392eee829f5f
lokesh keelisetti <keelisettilokesh@gmail.com>
2021-05-04 15:33:51 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port =  process.env.PORT || 3001;
+
+const uri = process.env.ATLAS_URI;
+
+const connect = mongoose.connect(uri,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true});
+
+connect.then((db) => {
+    console.log('MongoDB connected');
+},(err) => {
+    console.log(err);
+})
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port , () =>{
+    console.log(`Server is running on ${port}`);
+})
