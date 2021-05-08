76d2f1debd3ff3641b46308347e00a8bc14cf8d2
nunemunthala shiva <nunemunthalashiva12@gmail.com>
2021-05-08 12:52:39 +0530

    added hotel entity

---
+++ models/hotel.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const hotel = new Schema({
+  hotel_name:{
+    type:String,
+    required: true
+  },
+  rooms: [hotel_room]
+},{
+  timestamps: true
+})
