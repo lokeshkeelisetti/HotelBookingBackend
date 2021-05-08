eada3537e7c7cf1adc522e25ae8eb47755fcf8fc
nunemunthala shiva <nunemunthalashiva12@gmail.com>
2021-05-08 13:05:33 +0530

    Added receptionist model

---
+++ models/receptionist.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Receptionist = new Schema({
+  firstName:{
+    type:String,
+    required:true
+  },
+  lastName:{
+    type:String,
+    default:''
+  },
+  hotel:hotel,
+},{
+  timestamps:true
+})
