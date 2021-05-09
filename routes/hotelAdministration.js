a56644bbdf6206a6ccc7858cd5518e94e158f65b
nunemunthala shiva <nunemunthalashiva12@gmail.com>
2021-05-09 09:40:03 +0530

    added receptionist route in hotelAdmin

--- routes/hotelAdministration.js
+++ routes/hotelAdministration.js
@@ -20,3 +20,25 @@ router.route("/addRoom").post((req, res) => {
 		.then(() => res.json({ success: "Room added successfully" }))
 		.catch((err) => res.status(400).json({ failure: "Unable to add room", error: err }));
 });
+
+
+
+router.route("/").get((req, res) => {
+	HotelAdministration.find()
+		.then((hotelAdministrations) => res.json(hoteladministrations))
+		.catch((err) => res.status(400).json("Error finding admin" + err));
+});
+
+router.route("/addReceptionist").post((req, res) => {
+	firstName = req.body.firstName;
+	lastName = req.body.lastName;
+	email = req.body.email;
+	password = req.body.password; //adding default password later he can update
+	hotelId = req.body.hotelId;
+	const newReceptionist = new HotelRoom({ roomNo, hotelId,hotelType,"0"});//default status is '0' for not nookin
+
+	newRoom
+		.save()
+		.then(() => res.json({ success: "Receptionist added successfully" }))
+		.catch((err) => res.status(400).json({ failure: "Unable to add receptionist", error: err }));
+});
