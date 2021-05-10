bd73fb592d8a429dbc885ef882908ee5c01dfc89
nunemunthalashiva <shivanunemunthala@gmail.com>
2021-05-10 23:04:19 +0530

    removed some routes as they are not required

--- routes/receptionist.js
+++ routes/receptionist.js
@@ -9,18 +9,7 @@ const checkLogin = (userType, userSecret) => {
 	else return false;
 };
 
-//findRoom returns all the rooms whose status are free
-router.route("/findRoom").get((req, res) => {
-	if (checkLogin(req.headers.usertype, req.headers.usersecret)) {
-		HotelRoom.find()
-			.then((hotelRooms) => res.json(hotelRooms))
-			.catch((err) => res.json({ failure: "Unable to find rooms ", error: err }));
-	} else {
-		res.json({ failure: "Access Denied" });
-	}
-});
 
-// update roomStatus essentially mean booking room for offline people
 
 
 
