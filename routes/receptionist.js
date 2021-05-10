8cf312a07170465ff6dec89847998c6b9983c2c6
nunemunthalashiva <shivanunemunthala@gmail.com>
2021-05-10 22:28:20 +0530

    added check login to receptionist router

--- routes/receptionist.js
+++ routes/receptionist.js
@@ -1,6 +1,13 @@
 const router = require("express").Router();
 let Hotel = require("../models/hotel.model");
 
+
+// add RECEPTIONIST_SECRET to env file!!
+const checkLogin = (userType, userSecret) => {
+	if (userType == "receptionist" && userSecret == process.env.RECEPTIONIST_SECRET) return true;
+	else return false;
+};
+
 router.route("/").get((req, res) => {
 	Hotel.find()
 		.then((hotels) => res.json(hotels))
