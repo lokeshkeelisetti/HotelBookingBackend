147ea2f2d4bbc4a1447b3e237a0be445af8a2f42
Kushaan Gowda <kushaangowda@yahoo.com>
2021-05-10 21:02:57 +0530

    added authentication to maintainers route

--- routes/maintainer.js
+++ routes/maintainer.js
@@ -1,60 +1,94 @@
 const router = require("express").Router();
 let Hotel = require("../models/hotel.model");
 let HotelAdministration = require("../models/hotelAdministration.model");
+let Customer = require("../models/customer.model");
+
+const checkLogin = (userType, userSecret) => {
+	if (userType == "maintainer" && userSecret == process.env.MAINTAINER_SECRET) return true;
+	else return false;
+};
 
 router.route("/hotel").get((req, res) => {
-	Hotel.find()
-		.then((hotels) => res.json(hotels))
-		.catch((err) => res.json({ failure: "Unable to find hotels", error: err }));
+	if (checkLogin(req.headers.usertype, req.headers.usersecret)) {
+		Hotel.find()
+			.then((hotels) => res.json(hotels))
+			.catch((err) => res.json({ failure: "Unable to find hotels", error: err }));
+	} else {
+		res.json({ failure: "Access Denied" });
+	}
 });
 
 router.route("/hotelAdmin").get((req, res) => {
-	HotelAdministration.find()
-		.then((hotelAdmins) => res.json(hotelAdmins))
-		.catch((err) => res.json({ failure: "Unable to find hotel admins", error: err }));
+	if (checkLogin(req.headers.usertype, req.headers.usersecret)) {
+		HotelAdministration.find()
+			.then((hotelAdmins) => res.json(hotelAdmins))
+			.catch((err) => res.json({ failure: "Unable to find hotel admins", error: err }));
+	} else {
+		res.json({ failure: "Access Denied" });
+	}
+});
+
+router.route("/customers").get((req, res) => {
+	if (checkLogin(req.headers.usertype, req.headers.usersecret)) {
+		Customer.find()
+			.then((customers) => res.json(customers))
+			.catch((err) => res.json({ failure: "Unable to find hotel admins", error: err }));
+	} else {
+		res.json({ failure: "Access Denied" });
+	}
 });
 
 router.route("/addNewHotel").post((req, res) => {
-	const hotel = {
-		hotelName: req.body.hotelName,
-		address: {
-			street: req.body.street,
-			city: req.body.city,
-			pinCode: Number(req.body.pinCode),
-		},
-	};
+	if (checkLogin(req.headers.usertype, req.headers.usersecret)) {
+		const hotel = {
+			hotelName: req.body.hotelName,
+			address: {
+				street: req.body.street,
+				city: req.body.city,
+				pinCode: Number(req.body.pinCode),
+			},
+		};
 
-	const newHotel = new Hotel(hotel);
+		const newHotel = new Hotel(hotel);
 
-	newHotel
-		.save()
-		.then((hotel) => {
-			const hoteladmin = {
-				name: {
-					firstName: req.body.firstName,
-					lastName: req.body.lastName,
-				},
-				email: req.body.email,
-				password: req.body.password,
-				hotelId: hotel._id,
-			};
-			const newHotelAdministration = new HotelAdministration(hoteladmin);
-			newHotelAdministration
-				.save()
-				.then(() => res.json({ success: "Hotel Admin and Hotel added" }))
-				.catch((err) => res.json({ failure: "unable to add hotel admin", error: err }));
-		})
-		.catch((err) => res.json({ failure: "unable to add hotel ", error: err }));
+		newHotel
+			.save()
+			.then((hotel) => {
+				const hoteladmin = {
+					name: {
+						firstName: req.body.firstName,
+						lastName: req.body.lastName,
+					},
+					email: req.body.email,
+					password: req.body.password,
+					hotelId: hotel._id,
+				};
+				const newHotelAdministration = new HotelAdministration(hoteladmin);
+				newHotelAdministration
+					.save()
+					.then(() => res.json({ success: "Hotel Admin and Hotel added" }))
+					.catch((err) => res.json({ failure: "unable to add hotel admin", error: err }));
+			})
+			.catch((err) => res.json({ failure: "unable to add hotel ", error: err }));
+	} else {
+		res.json({ failure: "Access Denied" });
+	}
 });
 
 router.route("/removeHotel/:id").delete((req, res) => {
-	Hotel.findByIdAndDelete(req.params.id)
-		.then(() => {
-			HotelAdministration.findOneAndRemove({ hotelId: req.params.id })
-				.then(() => res.json("hotel admin and hotel deleted"))
-				.catch((err) => res.json({ failure: "Unable to delete hotel admin", error: err }));
-		})
-		.catch((err) => res.json({ failure: "Unable to delete hotel", error: err }));
+	if (checkLogin(req.headers.usertype, req.headers.usersecret)) {
+		Hotel.findByIdAndDelete(req.params.id)
+			.then(() => {
+				HotelAdministration.findOneAndRemove({ hotelId: req.params.id })
+					.then(() => res.json("hotel admin and hotel deleted"))
+					.catch((err) =>
+						res.json({ failure: "Unable to delete hotel admin", error: err })
+					);
+			})
+			.catch((err) => res.json({ failure: "Unable to delete hotel", error: err }));
+	} else {
+		res.json({ failure: "Access Denied" });
+	}
 });
 
 module.exports = router;
