f4c4d53746e7c57e8f645acfbd8d78c8b86dfe67
nunemunthalashiva <shivanunemunthala@gmail.com>
2021-05-10 22:40:51 +0530

    deleted default route in receptionist.js and added find hotel route

--- routes/receptionist.js
+++ routes/receptionist.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 let Hotel = require("../models/hotel.model");
-
+let Booking = require("../models/booking.model");
+let HotelRoom = require("../models/hotelRoom.model");
 
 // add RECEPTIONIST_SECRET to env file!!
 const checkLogin = (userType, userSecret) => {
@@ -8,11 +9,19 @@ const checkLogin = (userType, userSecret) => {
 	else return false;
 };
 
-router.route("/").get((req, res) => {
-	Hotel.find()
-		.then((hotels) => res.json(hotels))
-		.catch((err) => res.json("Error: " + err));
+//findRoom returns all the rooms whose status are free
+router.route("/findRoom").get((req, res) => {
+	if (checkLogin(req.headers.usertype, req.headers.usersecret)) {
+		HotelRoom.find()
+			.then((hotelRooms) => res.json(hotelRooms))
+			.catch((err) => res.json({ failure: "Unable to find rooms ", error: err }));
+	} else {
+		res.json({ failure: "Access Denied" });
+	}
 });
+
+// update roomStatus essentially mean booking room for offline people
+
 
 
 module.exports = router;
