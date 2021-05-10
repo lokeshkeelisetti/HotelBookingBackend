94cf13fcdd30c420beed80d96a9600f2a12cb93a
nunemunthalashiva <shivanunemunthala@gmail.com>
2021-05-10 23:39:10 +0530

    added updateStatus route to receptionist.js file

--- routes/receptionist.js
+++ routes/receptionist.js
@@ -9,8 +9,33 @@ const checkLogin = (userType, userSecret) => {
 	else return false;
 };
 
-
-
+//Confirm booking or update status in booking
+router.route("/updateStatus/:id").put((req, res) => {
+	if (checkLogin(req.headers.usertype, req.headers.usersecret)) {
+		Receptionist.findOne({ _id: req.body.receptionistId, hotelId: req.body.hotelId })
+			.then(() => {
+				Booking.findOne({ _id: req.params.id, hotelId: req.body.hotelId })
+					.then((booking) => {
+						booking.status = Boolean(req.body.status);
+						booking
+							.save()
+							.then(() => res.json({ success: "hotel room is confirmed!" }))
+							.catch((err) =>
+								res.json({ failure: "Unable to confirm hotel room", error: err })
+							);
+					})
+					.catch((err) =>
+						res.json({
+							failure: "Unable to find particular booking with given id",
+							error: err,
+						})
+					);
+			})
+			.catch((err) => res.json({ failure: "Unable to find hotel", error: err }));
+	} else {
+		res.json({ failure: "Access Denied" });
+	}
+});
 
 
 module.exports = router;
